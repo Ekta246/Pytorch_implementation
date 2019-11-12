b515ac6fea03ba622cc3eb88c84c9cb3cb50e9ad
Ekta246 <52435785+Ekta246@users.noreply.github.com>
2019-11-12 18:30:21 -0500

    Add files via upload
    Impro

--- MobNet_VDOT.py
+++ MobNet_VDOT.py
@@ -11,8 +11,8 @@ import torchvision.transforms as transforms
 import os
 import argparse
 
-from models import *
-from utils import progress_bar
+#from models import *
+#from utils import progress_bar
 
 parser = argparse.ArgumentParser(description='PyTorch Training')
 parser.add_argument('--lr', default=0.001, type=float, help='learning rate')
@@ -25,7 +25,7 @@ start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 
 # Data
 print('==> Preparing data..')
-transform_train = transforms.Compose([
+transform_train = transforms.Compose([transforms.Resize((224,224)),
     #transforms.RandomCrop(224, padding=4),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
@@ -36,15 +36,30 @@ transform_test = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
-
+#root = []
 trainset = torchvision.datasets.ImageFolder(root='./12-retrain/clean-dataset/train', transform=transform_train)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=, shuffle=True, num_workers=2)
-print(len(trainloader))
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=40, shuffle=True, num_workers=2)
+###print(len(trainloader))
+#type(trainset)
+#classification_categorical = []
+#if i in enumerate(trainset.classes)> 1:
+	     #classi=classification_categorical.append(len(i=1))
+#rainset.classes = classi
+		  		
+		
+print(trainset.classes)
+print(type(trainset.classes))
+for i in enumerate (trainset.classes):#need to sort the list
+	if len(i)> 1:
+	   print(i)
+
+print(trainset.class_to_idx)
+idx_to_class = {j:i for i,j in trainset.class_to_idx.items()}
+
 testset = torchvision.datasets.ImageFolder(root='./12-retrain/clean-dataset/validation',  transform=transform_test)
 testloader = torch.utils.data.DataLoader(testset, batch_size=10, shuffle=False, num_workers=2)
-print(len(testloader))
 
-classes = ('DR50', 'DR55', 'DR57', 'DR58', 'RO60', 'RO65', 'RS40', 'RS41', 'RS42', 'TR11', 'TR13', 'TR24')
+classes = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
 
 
 class Block(nn.Module):
@@ -89,11 +104,16 @@ class MobileNetV2(nn.Module):
     def __init__(self, num_classes=12):
         super(MobileNetV2, self).__init__()
         # NOTE: change conv1 stride 2 -> 1 for CIFAR10
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=2, padding=1, bias=False)
+	#print(LAYERS)
         self.bn1 = nn.BatchNorm2d(32)
         self.layers = self._make_layers(in_planes=32)
         self.conv2 = nn.Conv2d(320, 1280, kernel_size=1, stride=1, padding=0, bias=False)
         self.bn2 = nn.BatchNorm2d(1280)
+	#According to Sadegh, he took last layer as flattened and added a 512 layer and then fed it to softmax
+	#self.conv3 = nn.Conv2d(1280, 512, kernel_size=1, stride=1, padding=0, bias=False)
+	#self.bn3 = nn.BatchNorm2d(512)
+	#self.linear = nn.Linear(512, num_classes)
         self.linear = nn.Linear(1280, num_classes)
 
     def _make_layers(self, in_planes):
@@ -110,6 +130,7 @@ class MobileNetV2(nn.Module):
         out = self.layers(out)
         out = F.relu(self.bn2(self.conv2(out)))
         # NOTE: change pooling kernel_size 7 -> 4 for CIFAR10
+	#out = nn.Conv2d(in_planes, 512, )
         out = F.avg_pool2d(out, 4)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
@@ -211,4 +232,153 @@ def test():
     x = torch.randn(2,3,224,224)
     y = net(x)
     print(y.size())
+
+
+'''#  ---------------  Dataset  ---------------
+
+class StudentsPerformanceDataset(Dataset):
+    """Students Performance dataset."""
+
+    def __init__(self, train):
+        """Initializes instance of class StudentsPerformanceDataset.
+        Args:
+            csv_file (str): Path to the csv file with the students data.
+        """
+        #df = pd.read_csv(csv_file)
+
+        # Grouping variable names
+        self.categorical = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" ]
+        #self.target = "math score"
+
+        # One-hot encoding of categorical variables
+        #self.students_frame = pd.get_dummies(df, prefix=self.categorical)
+
+        # Save target and predictors
+        self.X = self.students_frame.drop(self.target, axis=1)
+        self.y = self.students_frame[self.target]
+
+    def __len__(self):
+        return len(self.students_frame)
+
+    def __getitem__(self, idx):
+        # Convert idx from tensor to list due to pandas bug (that arises when using pytorch's random_split)
+        if isinstance(idx, torch.Tensor):
+            idx = idx.tolist()
+
+        return [self.X.iloc[idx].values, self.y[idx]]
+
+
+#  ---------------  Model  ---------------
+
+class Net(nn.Module):
+
+    def __init__(self, D_in, H=15, D_out=1):
+        super().__init__()
+        self.fc1 = nn.Linear(D_in, H)
+        self.fc2 = nn.Linear(H, D_out)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+
+        return x.squeeze()
+
+
+#  ---------------  Training  ---------------
+
+def train(csv_file, n_epochs=100):
+    """Trains the model.
+    Args:
+        csv_file (str): Absolute path of the dataset used for training.
+        n_epochs (int): Number of epochs to train.
+    """
+    # Load dataset
+    dataset = StudentsPerformanceDataset(csv_file)
+
+    # Split into training and test
+    train_size = int(0.8 * len(dataset))
+    test_size = len(dataset) - train_size
+    trainset, testset = random_split(dataset, [train_size, test_size])
+
+    # Dataloaders
+    trainloader = DataLoader(trainset, batch_size=200, shuffle=True)
+    testloader = DataLoader(testset, batch_size=200, shuffle=False)
+
+    # Use gpu if available
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    # Define the model
+    D_in, H = 19, 15
+    net = Net(D_in, H).to(device)
+
+    # Loss function
+    criterion = nn.MSELoss()
+
+    # Optimizer
+    optimizer = optim.Adam(net.parameters(), weight_decay=0.0001)
+
+    # Train the net
+    loss_per_iter = []
+    loss_per_batch = []
+    for epoch in range(n_epochs):
+
+        running_loss = 0.0
+        for i, (inputs, labels) in enumerate(trainloader):
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            # Zero the parameter gradients
+            optimizer.zero_grad()
+
+            # Forward + backward + optimize
+            outputs = net(inputs.float())
+            loss = criterion(outputs, labels.float())
+            loss.backward()
+            optimizer.step()
+
+            # Save loss to plot
+            running_loss += loss.item()
+            loss_per_iter.append(loss.item())
+
+        loss_per_batch.append(running_loss / (i + 1))
+        running_loss = 0.0
+
+    # Comparing training to test
+    dataiter = iter(testloader)
+    inputs, labels = dataiter.next()
+    inputs = inputs.to(device)
+    labels = labels.to(device)
+    outputs = net(inputs.float())
+    print("Root mean squared error")
+    print("Training:", np.sqrt(loss_per_batch[-1]))
+    print("Test", np.sqrt(criterion(labels.float(), outputs).detach().cpu().numpy()))
+
+    # Plot training loss curve
+    plt.plot(np.arange(len(loss_per_iter)), loss_per_iter, "-", alpha=0.5, label="Loss per epoch")
+    plt.plot(np.arange(len(loss_per_iter), step=4) + 3, loss_per_batch, ".-", label="Loss per mini-batch")
+    plt.xlabel("Number of epochs")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+    import os
+    import sys
+    import argparse
+
+    # By default, read csv file in the same directory as this script
+    csv_file = os.path.join(sys.path[0], "StudentsPerformance.csv")
+
+    # Parsing arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file", "-f", nargs="?", const=csv_file, default=csv_file,
+                        help="Dataset file used for training")
+    parser.add_argument("--epochs", "-e", type=int, nargs="?", default=100, help="Number of epochs to train")
+    args = parser.parse_args()
+
+    # Call the main function of the script
+    train(args.file, args.epochs)'''
 
