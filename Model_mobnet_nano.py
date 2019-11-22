a03e0969be43e2bc54561b65c20ca3438a0a87fa
Ekta246 <ebhojwani246@gmail.com>
2019-11-21 22:45:03 -0500

    New

---
+++ Model_mobnet_nano.py
@@ -0,0 +1,139 @@
+import matplotlib.pyplot as plt
+import torchvision
+import numpy as np
+import torch
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+from torchvision import datasets, transforms, models
+from torchvision.models import mobilenet_v2, MobileNetV2
+from utils import progress_bar
+from datetime import datetime
+
+
+#data_dir  = '/Irma/train'
+best_acc = 0  # best test accuracy
+start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+
+print('==> Preparing data..')
+transform_train = transforms.Compose([transforms.Resize((224,224)),
+    #transforms.RandomCrop(224, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+#root = []
+train_data = torchvision.datasets.ImageFolder(root='./12-retrain/clean-dataset/train', transform=transform_train)
+trainloader = torch.utils.data.DataLoader(train_data, batch_size=40, shuffle=True, num_workers=2)
+print(len(trainloader))
+num_train = len(train_data)
+print(num_train)
+print(train_data.classes)
+#print(type(trainset.classes))
+
+
+print(train_data.class_to_idx)
+#idx_to_class = {j:i for i,j in trainset.class_to_idx.items()}
+
+test_data = torchvision.datasets.ImageFolder(root='./12-retrain/clean-dataset/validation',  transform=transform_test)
+testloader = torch.utils.data.DataLoader(test_data, batch_size=40, shuffle=False, num_workers=2)
+
+#trainloader, testloader = load_split_train_test(data_dir, .2)
+print(trainloader.dataset.classes)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Freeze parameters so we don't backprop through them
+tstart = datetime.now()
+
+model = torch.hub.load('pytorch/vision', 'mobilenet_v2', pretrained=True)
+
+#model=mobilenet_v2()
+#model = models.mobilenet_v2(pretrained=True)
+model
+
+for param in model.parameters():
+    param.requires_grad = False    
+
+model.classifier[1] = nn.Sequential(nn.Linear(in_features=model.classifier[1].in_features, out_features=512))#, nn.ReLU(),nn. Linear(512, 12), nn.Softmax(dim=1))
+#model.classifier[1]= nn.Linear(in_features=model.classifier[1].in_features, out_features=12)
+#model.classifier[2] = nn.Linear(in_features=model.classifier[2].in_features, out_features=12)
+print(model.classifier)
+
+model.to(device)
+
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.classifier.parameters(), lr=0.001, momentum=0.9)#, weight_decay=5e-4)
+
+# Training
+def train(epoch):
+    print('\nEpoch: %d' % epoch)
+    model.train()
+    train_loss = 0
+    correct = 0
+    total = 0
+    #train_losses, test_losses = [], []
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        inputs, targets = inputs.to(device), targets.to(device)
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+
+        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+            % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+      
+            
+def test(epoch):
+    global best_acc
+    model.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(testloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+
+            test_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+            
+#plt.plot(train_losses, label='Training loss')
+#plt.plot(test_losses, label='Validation loss')
+#plt.legend(frameon=False)
+#plt.show()
+
+for epoch in range(start_epoch, start_epoch+800):
+    train(epoch)
+    test(epoch)
+tend = datetime.now()
+
+delta = tend - tstart
+print('training time is', delta)
+torch.save(model.state_dict(), './Mob_classifier')
+print('model is saved')
+#for param_tensor in model.state_dict():
+    #print(param_tensor, "\t", model.state_dict()[param_tensor].size())
+
+model.load_state_dict(torch.load('./Mob_classifier'))
+#model.eval()
+
+
